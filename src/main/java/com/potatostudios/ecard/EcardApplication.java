e2df7ea4f4f5266f359eca2f239f90aa6df6b46f
sangwona <aswton@gmail.com>
2025-02-14 02:51:14 -0800

    dot.env condition added

--- src/main/java/com/potatostudios/ecard/EcardApplication.java
+++ src/main/java/com/potatostudios/ecard/EcardApplication.java
@@ -9,10 +9,18 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class EcardApplication {
 
 	public static void main(String[] args) {
+		// .env 파일에 값이 있을 경우에만 System property를 설정합니다.
 		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		if (dotenv.get("DB_URL") != null) {
+			System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		}
+		if (dotenv.get("DB_USERNAME") != null) {
+			System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		}
+		if (dotenv.get("DB_PASSWORD") != null) {
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		}
 
 		SpringApplication.run(EcardApplication.class, args);
 	}
